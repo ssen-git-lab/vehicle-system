bfabb11d0f465440bd65fb4ce611d4c9573bd5fe
inseong276-creator <inseong276@gmail.com>
2026-08-12 15:17:43 +0900

    INseong

--- src/vehicle-system.cpp
+++ src/vehicle-system.cpp
@@ -1,11 +1,30 @@
 ﻿// vehicle-system.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+// 
 //
-
+#include "airbag_actuator.h"
+#include "lightSystem.h"
+#include "wiper_system.h"
 #include <iostream>
 
 int main()
 {
     std::cout << "Hello World!\n";
+
+    RainSensor Rainsensor;
+    LightSensor lightSensor(0);
+
+
+    airbag_actuator airbag;
+    WiperSystem wiper(Rainsensor);
+    HeadLightSystem headlight(lightSensor);
+
+    while (1) {
+        airbag.Init(0 , 0);
+        airbag.Operate(0);
+
+        wiper.update();
+        headlight.update();
+    }
 }
 
 // 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
